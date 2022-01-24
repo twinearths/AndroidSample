2040c3525fe207698ce6510f2e775805da240832
twinearths <info@twinearths.co.jp>
2022-01-25 00:33:22 +0900

    add comment

--- app/src/main/java/jp/co/twinearths/androidsample/RecyclerAdapter.kt
+++ app/src/main/java/jp/co/twinearths/androidsample/RecyclerAdapter.kt
@@ -7,25 +7,30 @@ import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerAdapter(private val context: Context, private val list: MutableList<TableData>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
     companion object {
         const val HEADER_TYPE = 1
         const val ITEM_TYPE = 2
     }
-
+    // 全体のItemの数を設定
+    override fun getItemCount(): Int = list.size
+    // viewTypeを設定
+    override fun getItemViewType(position: Int): Int {
+        return list[position].viewType
+    }
+    // viewType: SectionとItemを使い分ける
+    // 要素のUIを作成
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-
-        if (viewType == HEADER_TYPE) {
-            return HeaderViewHolder(
+        return if (viewType == HEADER_TYPE) {
+            HeaderViewHolder(
                 LayoutInflater.from(context).inflate(R.layout.header_recycleview, parent, false)
             )
         } else  { // if (viewType == ITEM_TYPE)
-            return ItemViewHolder(
+            ItemViewHolder(
                 LayoutInflater.from(context).inflate(R.layout.item_recyclerview, parent, false)
             )
         }
     }
-
+    // それぞれのItemの中身を設定
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val itemData = list[position]
         if (itemData.viewType == RecyclerAdapter.HEADER_TYPE) {
@@ -36,10 +41,4 @@ class RecyclerAdapter(private val context: Context, private val list: MutableLis
             itemViewHolder.txtItem.text = itemData.textTitle
         }
     }
-
-    override fun getItemViewType(position: Int): Int {
-        return list[position].viewType
-    }
-
-    override fun getItemCount(): Int = list.size
 }
