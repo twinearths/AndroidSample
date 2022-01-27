580eebce9c7007ce4f6f435654a76c259b42e130
twinearths <info@twinearths.co.jp>
2022-01-27 12:27:27 +0900

    implement livedata event

--- app/src/main/java/jp/co/twinearths/androidsample/TopFragment.kt
+++ app/src/main/java/jp/co/twinearths/androidsample/TopFragment.kt
@@ -1,5 +1,4 @@
 package jp.co.twinearths.androidsample
-
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -7,41 +6,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextSwitcher
-import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.Navigation
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import jp.co.twinearths.androidsample.databinding.FragmentTopBinding
 
 class TopFragment : Fragment() {
-
-    private lateinit var recyclerView: RecyclerView
     private  lateinit var viewModel: TopViewModel
     private  lateinit var binding: FragmentTopBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         binding = FragmentTopBinding.inflate(layoutInflater)
         binding.lifecycleOwner = viewLifecycleOwner
         viewModel = ViewModelProvider(this).get(TopViewModel::class.java)
+        viewModel.inputMessage.observe(viewLifecycleOwner,
+            { value ->
+                binding.inputResultTextView.text = value
+            }
+        )
         binding.topViewModel = viewModel
 
         binding.inputText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 viewModel.setMessage(s.toString())
             }
-
             override fun afterTextChanged(s: Editable?) {}
         })
 
--- app/src/main/java/jp/co/twinearths/androidsample/TopViewModel.kt
+++ app/src/main/java/jp/co/twinearths/androidsample/TopViewModel.kt
@@ -1,14 +1,16 @@
 package jp.co.twinearths.androidsample
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TopViewModel: ViewModel() {
-    val inputMessage = MutableLiveData<String>()
-    val isNumber = MutableLiveData<Boolean>()
+    private val _inputMessage = MutableLiveData<String>()
+    val inputMessage: LiveData<String> = _inputMessage
 
+    val isNumber = MutableLiveData<Boolean>()
     fun setMessage(text:String) {
-        inputMessage.value = text
+        _inputMessage.value = text
         try {
             val num: Int = text.toInt()
             isNumber.value = true
